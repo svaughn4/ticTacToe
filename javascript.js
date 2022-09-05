cd18945b1dcb0070a5243d3bb7acd43b71b333fd
Syd <svaughn4@stanford.edu>
2022-09-05 15:28:02 -0700

    Add game functions and controller

--- javascript.js
+++ javascript.js
@@ -4,6 +4,15 @@ const gameboard = (() => {
     return {board}
 })();
 
+// Create game controller
+const controller = (() => {
+    // Turn property keeps track of who makes the current move
+    const turn = player1
+    // Status property remains false until someone wins or a tie
+    const status = false;
+    return{turn, status}
+})();
+
 // Keep track of the players
 let players = []
 
@@ -18,33 +27,58 @@ const playerFactory = (name) => {
     }
 }
 
+const player1 = playerFactory('Sydney')
+players.push(player1)
+const player2 = playerFactory('Vaughn') 
+players.push(player2)
+
 //console.log(gameboard.board)
 
 // Checks if the corresponding grid cell is clear in the gameboard array
 function checkMove(pos) { 
     for(let i = 0; i < gameboard.board.length; i++) {
         if (i == pos - 1 && gameboard.board[i] != undefined) {
-            return false;
+            return false
         }
-    }
-    return true;
+    } return true
+}
+
+// Returns true if a player has won otherwise returns false
+function hasWon() { 
+    const board = gameboard.board
+    // Check for row wins 
+    if(board[0] == board[1] == board[2] || board[3] == board[4] == board[5]
+        || board[6] == board[7] == board[8]) { 
+            return true
+        }
+    // Check for column wins
+    else if(board[0]== board[3] == board[6] || board[1] == board[4] == board[7] ||
+        board[2] == board[5] == board[8]) { 
+            return true
+    } 
+    // Check for diagonal wins
+    else if(board[0] == board[4] == board[8] || board[6] == board[4] == board[2]) {
+        return true
+    // Checks for a tie
+    } else if(board.every(element => element !== undefined)) { 
+        return true
+    // No one has won yet
+    } else return false
 }
 
 // Handles click event from players to occupy a new square
 function makeMove(element){ 
     // Get cell's id number to check with array
     let num = element.id
-    // Check if grid cell in array is already occupied
+    // Checks if player is able to make a move
     if(checkMove(num)) {
         gameboard.board[num-1] = 'hi'
-        element.textContent += 'hi'
+        element.textContent += `${num - 1}`
     }
-}
+    // Checks if after move a player has won
 
-const player1 = playerFactory('Sydney')
-players.push(player1)
-const player2 = playerFactory('Vaughn') 
-players.push(player2)
+    // Alters game controller to next player if game continues
+}
 
 // Add click event listener to each grid cell
 let grid = document.getElementsByClassName('cells')
